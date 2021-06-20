b1763aec6d0257a3f7f2ac316e8c1de10bd6f07c
Pantelis Tsakoulis <spideythewebhead@gmail.com>
2021-06-20 13:02:28 +0300

    - setup flutter app project

---
+++ app/android/app/src/main/kotlin/tsak/p/atm_locator/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package tsak.p.atm_locator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('App'),
+        ),
+      ),
+    );
+  }
+}
